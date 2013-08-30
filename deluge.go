ba4f9960f47aee07248fab8d9aae01104249ef28
Bruno Albuquerque <bga@bug-br.org.br>
2013-08-30 13:48:40 -0300

    Added initial code.

---
+++ deluge.go
@@ -0,0 +1,164 @@
+// Copyright 2013 Bruno Albuquerque (bga@bug-br.org.br).
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+// Package deluge implements a Go wrapper around the Deluge Remote JSON API
+// (http://deluge-torrent.org/docs/1.2/core/rpc.html#remote-api). This allows
+// programmers to control Deluge (http://deluge-torrent.org) programatically
+// from inside Go programs. Note this is a work in progress and not everything
+// is implemented but adding extra RPC calls is trivial.
+package deluge
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync/atomic"
+)
+
+// Deluge represents an endpoint for Deluge RPC requests.
+type Deluge struct {
+	url      string
+	password string
+
+	client  *http.Client
+	cookies []*http.Cookie
+
+	id uint64
+}
+
+// New instantiates a new Deluge instance and authenticates with the
+// server.
+func New(url, password string) (*Deluge, error) {
+	d := &Deluge{
+		url,
+		password,
+		new(http.Client),
+		nil,
+		0,
+	}
+
+	err := d.authLogin()
+	if err != nil {
+		return nil, err
+	}
+
+	return d, err
+}
+
+// CoreAddTorrentFile wraps the core.add_torrent_file RPC call. fileName is the
+// name of the original torrent file. fileDump is the base64 encoded contents of
+// the file and options is a map with options to be set (consult de Deluge
+// Torrent documentation for a list of valid options).
+func (d *Deluge) CoreAddTorrentFile(fileName, fileDump string, options map[string]interface{}) (string, error) {
+	response, err := d.sendJsonRequest("core.add_torrent_file", []interface{}{fileName, fileDump, options})
+	if err != nil {
+		return "", err
+	}
+
+	return response["result"].(string), nil
+}
+
+// CoreAddTorrentMagnet wraps the core.add_torrent_magnet RPC call. magnetUrl is
+// the Magnet URL for the torrent and options is a map with options to be set
+// (consult de Deluge Torrent documentation for a list of valid options).
+func (d *Deluge) CoreAddTorrentMagnet(magnetUrl string, options map[string]interface{}) (string, error) {
+	response, err := d.sendJsonRequest("core.add_torrent_magnet", []interface{}{magnetUrl, options})
+	if err != nil {
+		return "", err
+	}
+
+	return response["result"].(string), nil
+}
+
+// CoreAddTorrentUrl wraps the core.add_torrent_url RPC call. torrentUrl is
+// the URL for the torrent and options is a map with options to be set
+// (consult de Deluge Torrent documentation for a list of valid options).
+func (d *Deluge) CoreAddTorrentUrl(torrentUrl string, options map[string]interface{}) (string, error) {
+	response, err := d.sendJsonRequest("core.add_torrent_url", []interface{}{torrentUrl, options})
+	if err != nil {
+		return "", err
+	}
+
+	return response["result"].(string), nil
+}
+
+func (d *Deluge) authLogin() error {
+	response, err := d.sendJsonRequest("auth.login", []interface{}{d.password})
+	if err != nil {
+		return err
+	}
+
+	if response["result"] != true {
+		return fmt.Errorf("authetication failed")
+	}
+
+	return nil
+}
+
+func (d *Deluge) sendJsonRequest(method string, params []interface{}) (map[string]interface{}, error) {
+	atomic.AddUint64(&(d.id), 1)
+	data, err := json.Marshal(map[string]interface{}{
+		"method": method,
+		"id":     d.id,
+		"params": params,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", d.url, bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	if d.cookies != nil {
+		for _, cookie := range d.cookies {
+			req.AddCookie(cookie)
+		}
+	}
+
+	resp, err := d.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("received non-ok status to http request : %d", resp.StatusCode)
+	}
+
+	d.cookies = resp.Cookies()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make(map[string]interface{})
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	if result["error"] != nil {
+		return nil, fmt.Errorf("json error : %v", result["error"])
+	}
+
+	return result, err
+}
---
+++ example/add_torrent.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"flag"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	deluge "github.com/brunoga/go-deluge"
+)
+
+// Flags.
+var delugeAddress = flag.String("deluge_address", "127.0.0.1:8112", "deluge server address")
+var delugePassword = flag.String("deluge_password", "", "deluge server password")
+var torrent = flag.String("torrent", "", "torrent to add (url or file path)")
+var addPaused = flag.String("add_paused", false, "if true, torrent is added in the paused state")
+
+func main() {
+	flag.Parse()
+
+	if *delugeAddress == "" || *delugePassword == "" || *torrent == "" {
+		fmt.Printf("-deluge_address, -deluge_password and -torrent must all be provided and non-empty")
+		return
+	}
+
+	d, err := deluge.New("http://" + *delugeAddress + "/json", *delugePassword)
+	if err != nil {
+		panic(err)
+	}
+
+	options := map[string]interface{}{
+		"add_paused" : *addPaused,
+	}
+
+	var id string
+	if strings.HasPrefix(*torrent, "magnet:") {
+		id, err = d.CoreAddTorrentMagnet(*torrent, options)
+		if err != nil {
+			fmt.Println("Error adding torrent via magnet URL :", err)
+			return
+		}
+	} else if strings.Index(*torrent, "://") != -1 {
+		id, err = d.CoreAddTorrentUrl(*torrent, options)
+		if err != nil {
+			fmt.Println("Error adding torrent via URL :", err)
+		}
+	} else {
+		file, err :=  os.Open(*torrent)
+		if err != nil {
+			fmt.Println("Error opening local file :", err)
+			return
+		}
+
+		defer file.Close()
+
+		fileName := filepath.Base(*torrent)
+
+		data, err := ioutil.ReadAll(file)
+		if err != nil {
+			fmt.Println("Error reading local file :", err)
+			return
+		}
+
+		var buffer bytes.Buffer
+		encoder := base64.NewEncoder(base64.StdEncoding, &buffer)
+		encoder.Write(data)
+		encoder.Close()
+
+		id, err = d.CoreAddTorrentFile(fileName, buffer.String(), options)
+		if err != nil {
+			fmt.Println("Error adding torrent file :", err)
+		}
+	}
+
+	fmt.Println("Torrent added. Id : ", id)
+}
