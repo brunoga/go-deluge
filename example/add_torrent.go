81003f0103ea7ef05a940941e46198d2497c6715
Bruno Albuquerque <bga@bug-br.org.br>
2015-02-05 10:48:33 -0200

    Fixes and improvements.
    - Fix add_paused flag. This was preventing compilation of the code.
    - Accept torrents directly in the command line without without a specific flag.
    - Accept multiple torrents. at once.

--- example/add_torrent.go
+++ example/add_torrent.go
@@ -16,14 +16,13 @@ import (
 // Flags.
 var delugeAddress = flag.String("deluge_address", "127.0.0.1:8112", "deluge server address")
 var delugePassword = flag.String("deluge_password", "", "deluge server password")
-var torrent = flag.String("torrent", "", "torrent to add (url or file path)")
-var addPaused = flag.String("add_paused", false, "if true, torrent is added in the paused state")
+var addPaused = flag.Bool("add_paused", false, "if true, torrent is added in the paused state")
 
 func main() {
 	flag.Parse()
 
-	if *delugeAddress == "" || *delugePassword == "" || *torrent == "" {
-		fmt.Printf("-deluge_address, -deluge_password and -torrent must all be provided and non-empty")
+	if *delugeAddress == "" || *delugePassword == "" {
+		fmt.Printf("-deluge_address and -deluge_password must be provided and non-empty")
 		return
 	}
 
@@ -36,45 +35,48 @@ func main() {
 		"add_paused" : *addPaused,
 	}
 
-	var id string
-	if strings.HasPrefix(*torrent, "magnet:") {
-		id, err = d.CoreAddTorrentMagnet(*torrent, options)
-		if err != nil {
-			fmt.Println("Error adding torrent via magnet URL :", err)
-			return
-		}
-	} else if strings.Index(*torrent, "://") != -1 {
-		id, err = d.CoreAddTorrentUrl(*torrent, options)
-		if err != nil {
-			fmt.Println("Error adding torrent via URL :", err)
-		}
-	} else {
-		file, err :=  os.Open(*torrent)
-		if err != nil {
-			fmt.Println("Error opening local file :", err)
-			return
+	for _, torrent := range flag.Args() {
+		var id string
+		if strings.HasPrefix(torrent, "magnet:") {
+			id, err = d.CoreAddTorrentMagnet(torrent, options)
+			if err != nil {
+				fmt.Println("Error adding torrent via magnet URL :", err)
+				continue
+			}
+		} else if strings.Index(torrent, "://") != -1 {
+			id, err = d.CoreAddTorrentUrl(torrent, options)
+			if err != nil {
+				fmt.Println("Error adding torrent via URL :", err)
+				continue
+			}
+		} else {
+			file, err :=  os.Open(torrent)
+			if err != nil {
+				fmt.Println("Error opening local file :", err)
+				continue
+			}
+
+			defer file.Close()
+
+			fileName := filepath.Base(torrent)
+
+			data, err := ioutil.ReadAll(file)
+			if err != nil {
+				fmt.Println("Error reading local file :", err)
+				continue
+			}
+
+			var buffer bytes.Buffer
+			encoder := base64.NewEncoder(base64.StdEncoding, &buffer)
+			encoder.Write(data)
+			encoder.Close()
+
+			id, err = d.CoreAddTorrentFile(fileName, buffer.String(), options)
+			if err != nil {
+				fmt.Println("Error adding torrent file :", err)
+			}
 		}
 
-		defer file.Close()
-
-		fileName := filepath.Base(*torrent)
-
-		data, err := ioutil.ReadAll(file)
-		if err != nil {
-			fmt.Println("Error reading local file :", err)
-			return
-		}
-
-		var buffer bytes.Buffer
-		encoder := base64.NewEncoder(base64.StdEncoding, &buffer)
-		encoder.Write(data)
-		encoder.Close()
-
-		id, err = d.CoreAddTorrentFile(fileName, buffer.String(), options)
-		if err != nil {
-			fmt.Println("Error adding torrent file :", err)
-		}
+		fmt.Println("Torrent added. Id : ", id)
 	}
-
-	fmt.Println("Torrent added. Id : ", id)
 }
